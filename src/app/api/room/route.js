8afbf5760c4b6d12d8c57f2d33e988d26a27502a
Tanapat Choeichomsri <tanapat_choeichomsri@cmu.ac.th>
2023-09-14 14:08:09 +0700

    ลองรอบ 1

--- src/app/api/room/route.js
+++ src/app/api/room/route.js
@@ -7,31 +7,37 @@ export const GET = async () => {
   readDB();
   return NextResponse.json({
     ok: true,
-    //rooms:
-    //totalRooms:
+    rooms: true,
+    totalRooms: true
   });
 };
 
 export const POST = async (request) => {
-  const payload = checkToken();
-
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Invalid token",
-  //   },
-  //   { status: 401 }
-  // );
+  try{
+    const payload = checkToken();
+    roomId = payload.roomId;
+    role = payload.role;
+  } catch {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Invalid token",
+      },
+      { status: 401 }
+    );
+  }
+}
 
   readDB();
-
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: `Room ${"replace this with room name"} already exists`,
-  //   },
-  //   { status: 400 }
-  // );
+  const roomName = payload.roomName;
+  if(roomName=)
+  return NextResponse.json(
+    {
+      ok: false,
+      message: `Room ${"replace this with room name"} already exists`,
+    },
+    { status: 400 }
+  );
 
   const roomId = nanoid();
 
